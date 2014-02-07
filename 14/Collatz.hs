9d5aadb58bcd545d3cb7df91ffb5f67d22c42079
Fábio Roberto Teodoro <fabio.teodoro@serpro.gov.br>
2014-02-07 13:50:01 -0200

    Brute force solution for the Longest Collatz Sequence problem.

---
+++ 14/Collatz.hs
@@ -0,0 +1,14 @@
+-- http://projecteuler.net/problem=14
+
+import System.IO
+
+collatz_next :: Integer -> Integer
+collatz_next n
+	| n `mod` 2 == 0 = n `div` 2
+	| otherwise = 3 * n + 1
+
+collatz_chain_length :: Integer -> Integer
+collatz_chain_length 1 = 1
+collatz_chain_length n = 1 + collatz_chain_length (collatz_next n)
+
+main = putStrLn $ show $ maximum [collatz_chain_length n | n <- [1..1000000]]
