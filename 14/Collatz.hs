a307119c282e95d5d782c5b6dd9078b3c3c7782d
Fábio Roberto Teodoro <fabio.teodoro@serpro.gov.br>
2014-02-10 13:10:02 -0200

    Printing the number that produces the largest chain too.

--- 14/Collatz.hs
+++ 14/Collatz.hs
@@ -11,4 +11,4 @@ collatz_chain_length :: Integer -> Integer
 collatz_chain_length 1 = 1
 collatz_chain_length n = 1 + collatz_chain_length (collatz_next n)
 
-main = putStrLn $ show $ maximum [collatz_chain_length n | n <- [1..1000000]]
+main = putStrLn $ show $ maximum [(collatz_chain_length n, n) | n <- [1..1000000]]
