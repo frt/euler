0f8d82b913b9bc277ba8e3adeafd7ce81f7462eb
Fábio Roberto Teodoro <fabio.teodoro@serpro.gov.br>
2014-03-11 12:33:03 -0300

    Solution to the Maximum Path Sum II problem.

---
+++ 67/TrianglePath.hs
@@ -0,0 +1,27 @@
+-- http://projecteuler.net/problem=67
+
+import System.IO
+
+type Triangle = [[Integer]]
+
+triangleFromString :: String -> Triangle
+triangleFromString = map (map read . words) . lines
+
+maximumPathSum :: Triangle -> Triangle
+maximumPathSum t = maximumPathSum' 0
+	where
+		maximumPathSum' 0 = [t!!0!!0] : maximumPathSum' 1
+		maximumPathSum' r 
+			| r < length t = [greaterSum r c | c <- [0..r]] : maximumPathSum' (r+1)
+			| otherwise    = [] 
+
+		greaterSum r c = t!!r!!c + max (previousLeft) (previousRight)
+			where
+				previousLeft  = if c-1 < 0 then 0 else t'!!(r-1)!!(c-1)
+				previousRight = if c > r-1 then 0 else t'!!(r-1)!!c
+				t' = maximumPathSum t
+
+main = do
+	str <- getContents
+	let triangle = triangleFromString str
+	putStrLn $ show $ (maximum . last) (maximumPathSum triangle)
