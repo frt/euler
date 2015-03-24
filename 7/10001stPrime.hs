c432eaa50bed3b6d5234ee0d74ec73afbfc74f4f
Fábio Roberto Teodoro <fabio.teodoro@serpro.gov.br>
2015-03-24 11:34:10 -0300

    Solution to the 10001st Prime problem.

---
+++ 7/10001stPrime.hs
@@ -0,0 +1,20 @@
+-- http://projecteuler.net/problem=6
+
+import Euler (iSqrt)
+import System.IO (putStrLn)
+import System.Environment (getArgs)
+
+isPrime :: Integer -> Bool
+isPrime 1 = False
+isPrime 2 = True
+isPrime 3 = True
+isPrime n
+	| even n         = False
+	| n < 9          = True
+	| n `mod` 3 == 0 = False
+	| otherwise      = all ((/= 0) . (n `mod`)) [6 * k + l | k <- [1 .. (iSqrt n) `div` 6 + 1], l <- [-1, 1]]
+
+main = do
+	[arg1] <- getArgs
+	let n = read arg1
+	putStrLn $ show $ (filter isPrime [1..]) !! (n - 1)
